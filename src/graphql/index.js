77ef0c45d077dbdf4d3f9a075464401c98deb61e
ge-ge <wxj907468952@outlook.com>
2018-06-29 18:10:19 +0800

    save

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,16 +1,13 @@
 import ApolloClient from 'apollo-client'
-import { HttpLink } from 'apollo-link-http'
-import { ApolloLink, from } from 'apollo-link'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { HttpLink } from 'apollo-link-http'
+import { onError } from 'apollo-link-error'
+import { ApolloLink, from } from 'apollo-link'
 const token = '598ffa46592d1c7f57ccf8173e47290c6db0d549'
 
 const Middleware = new ApolloLink((operation, forward) => {
   // add the authorization to the headers
   console.log('Middleware', operation, forward)
-  return forward(operation).map(response => {
-    console.log('Middleware--response', response)
-    return response
-  })
 })
 const Afterware = new ApolloLink((operation, forward) => {
   return forward(operation).map(response => {
@@ -18,6 +15,16 @@ const Afterware = new ApolloLink((operation, forward) => {
     return response
   })
 })
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.map(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+      ),
+    );
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
 const httpLink = new HttpLink({
   uri: 'https://api.github.com/graphql',
   headers: {
@@ -26,6 +33,6 @@ const httpLink = new HttpLink({
 })
 const cache = new InMemoryCache()
 export default new ApolloClient({
-  link: from([Middleware, Afterware, httpLink]),
+  link: from([Middleware, Afterware, errorLink, httpLink]),
   cache
 })
